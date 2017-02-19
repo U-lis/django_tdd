70ae059ad1be25e347434d6b61a5815c9f5f1465
U-lis <ds2ilz@hotmail.com>
2017-02-20 00:35:34 +0900

    add werkzueg debugger. return html and pass tests.

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from django.utils import timezone
 
 
-def home_page():
-    pass
+def home_page(request):
+    return render(request, "home_page.html", {})
--- superlists/settings.py
+++ superlists/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django_extensions',
     'django_nose',
     'superlists',
+    "lists",
 ]
 
 MIDDLEWARE = [
